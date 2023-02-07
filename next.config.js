70accd814cbbd4c5346bd9b365ba42b3f9ef0d1d
Luiz de Freitas <luizdefreitas10@gmail.com>
2023-02-07 15:55:49 -0300

    oidjasioj

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  // next.js config
+})
+
+// module.exports = {
+//   reactStrictMode: true,
+// };
+
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     disable: process.env.NODE_ENV === "development",
+//     skipWaiting: true,
+//   },
+// });
